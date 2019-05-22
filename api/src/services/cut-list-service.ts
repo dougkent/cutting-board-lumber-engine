bf17c1e3d07db8dd74be0164cdf7762bd46f775f
Doug Kent <doug.kent88@gmail.com>
2019-05-22 05:39:50 -0500

    Adding outline of business logic.

--- api/src/services/cut-list-service.ts
+++ api/src/services/cut-list-service.ts
@@ -10,6 +10,14 @@ export class CutListService {
         blockWidth: number,
         blockDepth: number
     ): ICutList {
+
+        // TODO: Add business logic here
+        // 1. Determine Board thickness based on block width
+        // 2. Determine Board Volume based on dimensions passed in
+        // 3. Round up Board Volume up to nearest whole number and add a fudge factor for milling and saw kerf to get board feet
+        // 4. Determine dimensions for pieces to be milled
+        // 5. Determine dimensions for pieces to be cut after glued up
+
         return {
             boardFeet: 12,
             boardSize: BoardSizeEnum.FourQtr,
